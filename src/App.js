ea52c52fb398b7e506a8eb1739ee5192859293df
lakerka <zilvarudzionis@gmail.com>
2018-04-06 17:22:00 +0300

    Complete first task.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
 import './App.css';
+
+import { Task1App } from './task-1/user-actions';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>This is my React app!</h1>
+          <Task1App />
       </div>
     );
   }
---
+++ src/task-1/user-actions.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import _ from 'lodash';
+
+import './user-actions.css';
+
+
+export class Task1App extends React.Component {
+    state = {
+        people: [
+            {username: 'Ben'},
+            {username: 'Laura'},
+            {username: 'Ralf'}
+        ]
+    };
+
+    changeState = event => {
+        const people = _.clone(this.state.people);
+        people[1].username = event.target.value;
+        this.setState(people);
+    };
+
+    render() {
+        return (
+            <div className="Task1App">
+                <UserInput username={this.state.people[1].username} onChange={this.changeState} />
+                {this.state.people.map(person =>
+                    <UserOutput key={person.username} username={person.username} />
+                )}
+            </div>
+        );
+    }
+}
+
+export class UserInput extends React.Component {
+    render() {
+        return (
+            <input value={this.props.username} onChange={this.props.onChange}></input>
+        );
+    }
+}
+
+
+export class UserOutput extends React.Component {
+    render() {
+        return (
+            <div className="UserOutput">
+                <p>{this.props.username}</p>
+                <p></p>
+            </div>
+        );
+    }
+}
+
+
