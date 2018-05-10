f52afa513d75c91dd94160cc90174e6c8bfa3107
lakerka <zilvarudzionis@gmail.com>
2018-05-10 12:44:16 +0300

    Add lazy loading.

--- src/App.js
+++ src/App.js
@@ -3,14 +3,26 @@ import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.css';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
 
+
+const asyncCheckout = asyncComponent(() => {
+   return import('./containers/Checkout/Checkout');
+});
+
+
+const asyncOrders = asyncComponent(() => {
+   return import('./containers/Orders/Orders');
+});
+
+
+const asyncAuth = asyncComponent(() => {
+   return import('./containers/Auth/Auth');
+});
 
 class App extends Component {
     componentDidMount() {
@@ -23,7 +35,7 @@ class App extends Component {
 
         const unauthenticatedRoutes = (
             <Switch>
-                <Route path="/auth" component={Auth} />
+                <Route path="/auth" component={asyncAuth} />
                 <Route path="/" exact component={BurgerBuilder} />
                 <Redirect to="/" />
                 { allowRedirect && <Redirect to="/" /> }
@@ -32,10 +44,10 @@ class App extends Component {
 
         const authenticatedRoutes = (
             <Switch>
-                <Route path="/auth" component={Auth} />
+                <Route path="/auth" component={asyncAuth} />
                 <Route path="/logout" component={Logout} />
-                <Route path="/checkout" component={Checkout} />
-                <Route path="/orders" component={Orders} />
+                <Route path="/checkout" component={asyncCheckout} />
+                <Route path="/orders" component={asyncOrders} />
                 <Route path="/" exact component={BurgerBuilder} />
                 { allowRedirect && <Redirect to="/" /> }
             </Switch>
